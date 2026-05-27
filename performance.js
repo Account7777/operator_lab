a0c5feb6bda3afc5ef5505b55d0857bb4724d973
Mike <mvilchynskyy@gmail.com>
2026-05-27 18:16:30 +0300

    Updating performance.js

--- performance.js
+++ performance.js
@@ -3,5 +3,8 @@ const scores = [85, 87, 90, 94, 88];
 // .reduce() adds each number (b) to the total (a)
 const avg = scores.reduce((a, b) => a + b, 0) / scores.length;
 // Conditional Logic (Ternary Operator)
-const result = avg > 95 ? "Meeting expectations" : "Needs improvement"
-console.log(`Average: ${avg} - ${result}`);
+scores.forEach((score) => {
+    const result = score > avg ? "Meeting Expectations" : "Needs Improvement";
+    console.log(`Average: ${avg}, Score: ${score} - ${result}`);
+});
+
